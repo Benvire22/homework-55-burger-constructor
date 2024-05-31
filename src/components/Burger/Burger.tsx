7720bd82bf55338b9af21b10cebd0672b7a7c119
Limarev Ivan <ivantaketo@gmail.com>
2024-06-01 02:02:16 +0600

    Add .editorconfig file, add styles

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,37 +1,43 @@
-import "./Burger.css";
-import React from "react";
-
+import './Burger.css';
+import React from 'react';
 
 interface Ingredient {
-    name: string;
-    count: number;
+  name: string;
+  count: number;
 }
 
 interface Props {
-    ingredients: Ingredient[];
+  ingredients: Ingredient[];
+  price: () => number;
 }
 
-const Burger: React.FC<Props> = ({ingredients}) => {
+const Burger: React.FC<Props> = ({ingredients, price}) => {
 
-    const addIngredient = (ingredients: Ingredient[]) => {
-        return ingredients.reduce((acc: React.ReactNode[], current) => {
-            for (let i = 0; i < current.count; i++) {
-                acc.push(<div key={current.count + current.name + i} className={current.name}></div>);
-            }
-            return acc;
-        }, []);
-    };
+  const addIngredient = (ingredients: Ingredient[]) => {
+    return ingredients.reduce((acc: React.ReactNode[], current) => {
+      for (let i = 0; i < current.count; i++) {
+        acc.push(<div key={current.count + current.name + i} className={current.name}></div>);
+      }
 
-    return (
-        <div className="Burger">
-            <div className="BreadTop">
-                <div className="Seeds1"></div>
-                <div className="Seeds2"></div>
-            </div>
-            {addIngredient(ingredients)}
-            <div className="BreadBottom"></div>
+      return acc;
+    }, []);
+  };
+
+  return (
+    <div className="col burgerBlock">
+      <h2 className="title">Burger</h2>
+      <div className="Burger">
+        <div className="BreadTop">
+          <div className="Seeds1"></div>
+          <div className="Seeds2"></div>
         </div>
-    );
+        {addIngredient(ingredients)}
+        <div className="BreadBottom"></div>
+      </div>
+      <h3 className="burgerPrice">Price: {price()}.</h3>
+    </div>
+
+  );
 };
 
 export default Burger;
---
+++ src/components/IngredientItem/IngredientItem.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './IngredientItem.css';
+
+interface Props {
+  name: string;
+  price: number;
+  image: string;
+  count: number;
+  addIngredient: () => void;
+  removeIngredient: () => void;
+}
+
+const IngredientItem: React.FC<Props> = ({name, price, image, count, addIngredient, removeIngredient}) => {
+  return (
+    <div className="Ingredient">
+      <div className="Ingredient-info">
+        <img onClick={addIngredient} className="Ingredient-image" src={image} alt={name}/>
+        <span className="Ingredient-name">{name}</span>
+        <strong className="Ingredient-price">Price {price}</strong>
+        <span className="Ingredient-count">x{count}</span>
+        <div className="remove-ingredient">
+          {count > 0 ? (
+            <button className="remove-btn" onClick={removeIngredient} type="button"></button>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IngredientItem;
