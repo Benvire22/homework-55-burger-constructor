f1de4db46c225df74d4aa2c7baf9569f27f07cc6
Limarev Ivan <ivantaketo@gmail.com>
2024-06-01 00:31:03 +0600

    Update project

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -10,15 +10,16 @@ interface Ingredient {
 interface Props {
     ingredients: Ingredient[];
 }
+
 const Burger: React.FC<Props> = ({ingredients}) => {
-    const getIngredient = (name: string, count: number) => {
-        const ingredients: React.ReactNode[] = [];
 
-        for (let i = 0; i < count; i++) {
-            ingredients.push(<div className={name}></div>);
-        }
-
-        return ingredients;
+    const addIngredient = (ingredients: Ingredient[]) => {
+        return ingredients.reduce((acc: React.ReactNode[], current) => {
+            for (let i = 0; i < current.count; i++) {
+                acc.push(<div key={current.count + current.name + i} className={current.name}></div>);
+            }
+            return acc;
+        }, []);
     };
 
     return (
@@ -27,7 +28,7 @@ const Burger: React.FC<Props> = ({ingredients}) => {
                 <div className="Seeds1"></div>
                 <div className="Seeds2"></div>
             </div>
-            {ingredients.map(({name, count}) => getIngredient(name, count))}
+            {addIngredient(ingredients)}
             <div className="BreadBottom"></div>
         </div>
     );
